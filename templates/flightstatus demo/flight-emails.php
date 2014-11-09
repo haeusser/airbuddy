75cbef2460f9f20f3f9174aff1803c8bf426a5c1
coinlex <post@coinlex.com>
2014-11-09 09:33:07 +0100

    Added success.html, minor changes in main.py

---
+++ templates/flightstatus demo/flight-emails.php
@@ -0,0 +1,43 @@
+
+
+<a href="flight-emails.php?send=invitation">Send E-Mail: Invitation</a>
+<br>
+<br>
+<a href="flight-emails.php?send=delayed">Send E-Mail: Delayed</a>
+<br>
+<br>
+<a href="flight-emails.php?send=soon">Send E-Mail: Soon</a>
+<br>
+<br>
+<a href="flight-emails.php?send=arrival">Send E-Mail: Arrival</a>
+<br>
+<br>
+<br>
+<br>
+
+
+<?php
+
+$send = $_GET['send'];
+
+/* --- E-Mail: Invitation --- */
+// mail();
+
+/* --- E-Mail: Delayed --- */
+// mail();
+
+/* --- E-Mail: Delayed --- */
+// mail();
+
+/* --- E-Mail: Delayed --- */
+// mail();
+
+echo $send;
+
+
+?>
+
+
+
+
+
